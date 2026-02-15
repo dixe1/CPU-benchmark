fa6b671213c8be275d98aa1b4815782de73a0e8d
Bartłomiej Dobrowolski <annim1.5.1@gmail.com>
2026-02-15 14:21:48 +0100

    add: loading bar

--- includes/Benchmark.h
+++ includes/Benchmark.h
@@ -8,6 +8,7 @@
 #include <thread>
 #include <chrono>
 #include <cmath>
+#include <atomic>
 
 #include "../includes/Benchmark.h"
 #include "../includes/ConfigLoader.h"
@@ -18,17 +19,22 @@ private:
     const std::unordered_map<std::string, std::string> config = ConfigLoader::load("../config/config.txt");
     void calculate(size_t threads)
     {
-        const uint32_t cycles = std::floor(std::stoi(config.at("cycles")) / threads);
+        const uint64_t cycles = std::floor(std::stoi(config.at("cycles")) / threads);
 
-        for (uint32_t i{}; i < cycles; i++)
+        double j{};
+        for (uint64_t i{}; i < cycles; i++)
         {
-            volatile double r = std::pow(std::stod(config.at("num1")), std::stod(config.at("num2")));
+            volatile double r = std::sin(std::stod(config.at("num")) + j + 1);
+            j += 0.001;
         }
     }
 
 public:
-    double startBenchmark(size_t threadsToUse)
+    double duration{};
+
+    void startBenchmark(size_t threadsToUse, std::atomic<bool>& isWorking)
     {
+        isWorking = true;
         std::vector<std::thread> threads;
         threads.reserve(threadsToUse);
 
@@ -41,7 +47,8 @@ public:
 
         auto end = std::chrono::high_resolution_clock::now();
 
-        std::chrono::duration<double> duration = end - start;
-        return duration.count();
+        std::chrono::duration<double> duration_ = end - start;
+        isWorking = false;
+        duration = duration_.count();
     };
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <atomic>
+#include <array>
+#include <chrono>
+#include "unordered_map"
 
 #include "../includes/Menu.h"
 #include "../includes/GetUserInput.h"
 #include "../includes/ClearTerminal.h"
 #include "../includes/Benchmark.h"
+#include "../includes/ConfigLoader.h"
 
 int main()
 {
+    std::vector<std::thread> threads;
+    std::atomic<bool> isWorking{true};
+    Benchmark benchmark;
+
     bool repeatMenu = true;
     while (repeatMenu)
     {
@@ -15,24 +25,50 @@ int main()
         printMenu();
         std::cout << ">> ";
         const auto userInput = getUserInput();
-        Benchmark b1;
         double duration{};
 
         switch (userInput)
         {
             case '1':
-                duration = b1.startBenchmark(std::thread::hardware_concurrency());
+                threads.emplace_back(
+                    &Benchmark::startBenchmark,
+                    &benchmark,
+                    std::thread::hardware_concurrency(),
+                    std::ref(isWorking)
+                );
                 repeatMenu = false;
                 break;
 
             case '2':
-                duration = b1.startBenchmark(1);
+                threads.emplace_back(
+                    &Benchmark::startBenchmark,
+                    &benchmark,
+                    1,
+                    std::ref(isWorking)
+                );
                 repeatMenu = false;
                 break;
-
             case '3':
                 return 0;
         }
-        std::cout << duration << "s" << std::endl;
+
+        std::array<std::string, 6> loading {".","..","...", "....", ".....", "......"};
+        int i{};
+        while (isWorking)
+        {
+            if (i > 5) i = 0;
+            std::cout << loading.at(i) << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(300));
+            clearTerminal();
+            i++;
+        }
+
+        threads[0].join();
+        auto config = ConfigLoader::load("config/config.txt");
+
+        std::cout << "======= DATA =======\n";
+        std::cout << "cycles: " << config.at("cycles") << "\n";
+        std::cout << "num: " << config.at("num") << "\n";
+        std::cout << "duration [less - better]: " << benchmark.duration << "s\n";
     }
 }
