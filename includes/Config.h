f552feeb6170002430ebe1dd48b1ec60c5f9d241
Bartłomiej Dobrowolski <135438002+dixe1@users.noreply.github.com>
2026-09-16 19:49:07 +0200

    fix: typo error in Config.h, and clean CMakeLists.txt

--- includes/Config.h
+++ includes/Config.h
@@ -11,5 +11,5 @@ namespace Config
 inline constexpr uint64_t cycles = 7000000000;
 inline constexpr double num = 3.123141;
 
-static_assert(cycles >= 50, "value of cycles must be at least 400!");
+static_assert(cycles >= 400, "value of cycles must be at least 400!");
 } // namespace Config
