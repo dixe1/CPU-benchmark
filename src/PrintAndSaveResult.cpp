63eac647750b0946450c9af7eeda3f7eaf4d4f45
Bartłomiej Dobrowolski <135438002+dixe1@users.noreply.github.com>
2026-09-17 10:43:45 +0200

    refactor: simplify output()

--- src/PrintAndSaveResult.cpp
+++ src/PrintAndSaveResult.cpp
@@ -40,7 +40,7 @@ struct Colors
     const std::string BOLD = "\033[1m";
 
     static Colors ON() { return {}; }
-    static Colors OFF() { return {"", "", "", "", "", ""}; }
+    static Colors OFF() { return {.GREEN = "", .CYAN = "", .YELLOW = "", .RESET = "", .DIM = "", .BOLD = ""}; }
 };
 } // namespace
 
@@ -48,50 +48,13 @@ void printAndSaveResult(const Application& app)
 {
     auto output = [&](std::ostream& out, const Colors& c)
     {
-        auto header = [&]()
-        {
-            return std::format("{}{}BENCHMARK REPORT{}\n", c.BOLD, c.GREEN,
-                               c.RESET);
-        };
-        auto separator = [&]()
-        {
-            return std::format(
-                "{}──────────────────────────────────────────{}\n", c.DIM,
-                c.RESET);
-        };
-        auto CPU = [&]()
-        {
-            return std::format("{}{}CPU{}: {}\n", c.BOLD, c.YELLOW, c.RESET,
-                               getCPUName());
-        };
-        auto cycles = [&]()
-        {
-            return std::format("{}{}cycles{}: {}\n", c.BOLD, c.YELLOW, c.RESET,
-                               Config::cycles);
-        };
-        auto num = [&]()
-        {
-            return std::format("{}{}num{}: {}\n\n", c.BOLD, c.YELLOW, c.RESET,
-                               Config::num);
-        };
-        auto duration = [&]()
-        {
-            return std::format("{}{}duration{}: {} seconds\n", c.BOLD, c.YELLOW,
-                               c.RESET, app.getBenchmarkDuration());
-        };
-        auto score = [&]()
-        {
-            return std::format("{}{}score{}: {} points\n", c.BOLD, c.YELLOW,
-                               c.RESET, app.getBenchmarkPoints());
-        };
-
-        out << header();
-        out << separator();
-        out << CPU();
-        out << cycles();
-        out << num();
-        out << duration();
-        out << score();
+        out << std::format("{}{}BENCHMARK REPORT{}\n",     c.BOLD, c.GREEN,c.RESET);
+        out << std::format("{}──────────────────────────────────────────{}\n", c.DIM,c.RESET);
+        out << std::format("{}{}CPU{}: {}\n",              c.BOLD, c.YELLOW, c.RESET, getCPUName());
+        out << std::format("{}{}cycles{}: {}\n",           c.BOLD, c.YELLOW, c.RESET, Config::cycles);
+        out << std::format("{}{}num{}: {}\n\n",            c.BOLD, c.YELLOW, c.RESET, Config::num);
+        out << std::format("{}{}duration{}: {} seconds\n", c.BOLD, c.YELLOW, c.RESET, app.getBenchmarkDuration());
+        out << std::format("{}{}score{}: {} points\n",     c.BOLD, c.YELLOW, c.RESET, app.getBenchmarkPoints());
     };
 
     // Print result to console
